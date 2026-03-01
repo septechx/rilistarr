8b584f34e1bc3dc375cbeefbb369282dead9e5e6
Sep <septechx@proton.me>
2026-03-01 18:45:42 +0100

    feat: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::result::Result as StdResult;
+use std::{env, fs::read_to_string, io};
+
+use brawl_api::{Client, Player, traits::PropFetchable};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum RilistarrError {
+    #[error("failed to query the brawl stars API: {0}")]
+    BrawlApi(String),
+    #[error("failed to load .env: {0}")]
+    DotEnvLoad(#[from] dotenvy::Error),
+    #[error("failed to read BRAWL_TOKEN")]
+    ReadToken(#[from] env::VarError),
+    #[error("failed to read config")]
+    ReadConfig(#[from] io::Error),
+    #[error("failed to parse config")]
+    ParseConfig(#[from] serde_json::Error),
+}
+
+impl From<brawl_api::Error> for RilistarrError {
+    fn from(value: brawl_api::Error) -> Self {
+        Self::BrawlApi(value.to_string())
+    }
+}
+
+type Result<T> = StdResult<T, RilistarrError>;
+
+fn main() -> Result<()> {
+    dotenvy::dotenv()?;
+
+    let token = env::var("BRAWL_TOKEN")?;
+    let client = Client::new(&token);
+
+    let data = read_to_string("data.json")?;
+    let ids = serde_json::from_str::<Box<[&str]>>(&data)?;
+
+    let mut players = ids
+        .iter()
+        .map(|id| Player::fetch(&client, id))
+        .collect::<StdResult<Vec<_>, brawl_api::Error>>()?;
+
+    players.sort_by(|p1, p2| p2.trophies.cmp(&p1.trophies));
+
+    let text = players
+        .iter()
+        .enumerate()
+        .map(|(i, p)| format!("({}) {}: {} trophies\n", i + 1, p.name, p.trophies))
+        .collect::<String>();
+
+    println!("{}", text);
+
+    Ok(())
+}
